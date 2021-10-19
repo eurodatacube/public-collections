b0e977e586a59831100a4c700f71523e87f8f159
Dorothyrono <ronodorothy@yahoo.com>
2021-10-19 14:51:31 +0200

    remove extra spaces

--- collections/worldcover/script.js
+++ collections/worldcover/script.js
@@ -19,11 +19,11 @@ const map = [
     [40, 0xf096ff], // Cropland
     [50, 0xfa0000], // Built up
     [60, 0xb4b4b4], // Bare / sparse vegetation
-    [70, 0xf0f0f0], //	Snow and Ice
-    [80, 0x0064c8], //	Permanent water bodies
-    [90, 0x0096a0], //	Herbaceous wetland
-    [95, 0x00cf75], //	Mangroves
-    [100, 0xfae6a0], //	Moss and lichen
+    [70, 0xf0f0f0], // Snow and Ice
+    [80, 0x0064c8], // Permanent water bodies
+    [90, 0x0096a0], // Herbaceous wetland
+    [95, 0x00cf75], // Mangroves
+    [100, 0xfae6a0], // Moss and lichen
     
   ];
 
@@ -32,4 +32,3 @@ const visualizer = new ColorMapVisualizer(map);
 function evaluatePixel(sample) {
   return [visualizer.process(sample.Map)[0], visualizer.process(sample.Map)[1], visualizer.process(sample.Map)[2], sample.dataMask];
 }
-
