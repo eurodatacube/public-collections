d36348b7193faa85c94b64b25902bb459f1ebdf5
Pontus Berg <pontusberg95@gmail.com>
2021-11-22 09:33:48 +0100

    Added check for stac folder to make tests not crash if it doesnt exist

--- stac-json.test.js
+++ stac-json.test.js
@@ -3,14 +3,35 @@ const fs = require('fs');
 const path = require('path');
 
 const jsonDir = './_output/stac/';
-const files = fs.readdirSync(jsonDir);
+
+let testif; 
+let files; 
+
+if(isExistingDirectory(jsonDir)) { 
+    testif = test
+    files = fs.readdirSync(jsonDir);
+} else { 
+    testif = test.skip
+    files = []
+}
 
 files.forEach((file) => {
-    test(`.Should parse ${file} in STAC folder as JSON.`, () => {
+    testif(`Should parse ${file} in STAC folder as JSON.`, () => {
         const buffer = fs.readFileSync(path.join(jsonDir, file));
         expect(isValidJSON(buffer)).toBe(true)
     })
 })
+
+test.skip("Workaround to have at least one test available", () => true)
+
+function isExistingDirectory(directory) {
+    try {
+        fs.readdirSync(directory);
+    } catch (error) {
+        return false
+    }
+    return true
+}
 
 function isValidJSON(buffer) {
     try {
@@ -23,4 +44,4 @@ function isValidJSON(buffer) {
     }
 
     return true
-} 
+}
